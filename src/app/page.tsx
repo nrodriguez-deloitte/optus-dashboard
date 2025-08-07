27812d4ff1e4f6c6470f56afe330b18b80368c5c
Nino Rodriguez <nirodriguez@deloitte.com.au>
2025-08-07 11:14:39 +1000

    feat: mapped OutageTabs into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 import { Navigation } from "./components/organisms/Navigation";
 import { Map } from "./components/organisms/Map";
+import { OutageTabs } from "./components/organisms/OutageTabs";
 
 export default function Home() {
   return (
-    <main className="flex h-screen">
+    <main className="flex h-full">
       <div className="navigation-sidebar max-w-3xs w-full">
         <Navigation id="navigation" />
       </div>
 
       <div className="content p-[24px] w-full">
         <Map id="map" />
+
+        <OutageTabs id="outage-tabs" />
       </div>
+
       {/* <div>Side Panel</div> */}
     </main>
   );
