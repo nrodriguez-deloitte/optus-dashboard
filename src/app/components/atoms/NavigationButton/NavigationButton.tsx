2c095ff9bc191e1541bfdb40841c11ddd0620301
Nino Rodriguez <nirodriguez@deloitte.com.au>
2025-10-14 13:38:24 +1100

    feat: add NavigationButton component with styles and props definition

---
+++ src/app/components/atoms/NavigationButton/NavigationButton.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./NavigationButton.styles.scss";
+import { NavigationButtonProps } from "./NavigationButton.types";
+
+const NavigationButton: React.FC<NavigationButtonProps> = ({
+  children,
+  active = false,
+  disabled = false,
+  icon,
+  href,
+  "aria-label": ariaLabel,
+  ...rest
+}) => {
+  console.log(icon);
+  return (
+    <button
+      className={`navigation-button ${
+        active ? " navigation-button--active" : ""
+      }${disabled ? " navigation-button--disabled" : ""}`}
+      aria-label={ariaLabel}
+      aria-current={active ? "page" : undefined}
+      disabled={disabled}
+      {...rest}
+    >
+      <span className="navigation-button__label">{children}</span>
+
+      {/* // * TODO: Add icon rendering */}
+      {icon && <span className="navigation-button__icon">{icon}</span>}
+    </button>
+  );
+};
+
+export default NavigationButton;
---
+++ src/app/components/atoms/NavigationButton/NavigationButton.types.ts
@@ -0,0 +1,8 @@
+export interface NavigationButtonProps {
+  children?: React.ReactNode;
+  active?: boolean;
+  disabled?: boolean;
+  icon?: React.ReactNode;
+  href?: string;
+  "aria-label"?: string;
+}
