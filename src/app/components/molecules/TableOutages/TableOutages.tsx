e06d81e310d6e40e47eb5f3a42d06ec38894db2a
Nino Rodriguez <nirodriguez@deloitte.com.au>
2025-10-15 14:24:37 +1100

    feat: add TableOutages component with data display and styling

---
+++ src/app/components/molecules/TableOutages/TableOutages.tsx
@@ -0,0 +1,80 @@
+import { FilterIcon } from "lucide-react";
+import { MaterialReactTable, MRT_ColumnDef, MRT_TableOptions } from "material-react-table";
+import React, { useMemo } from "react";
+
+import { IOutageProps, useDataOutage } from "@/app/contexts/DataContext";
+import { formatCreationTime, isoToDateTime, toTitleCase } from "@/lib/utils";
+
+import "./TableOutages.styles.scss";
+import { TableOutagesProps } from "./TableOutages.types";
+
+const TableOutages: React.FC<TableOutagesProps> = () => {
+  const { outageData, loading } = useDataOutage();
+
+  const columns = useMemo<MRT_ColumnDef<IOutageProps>[]>(
+    () => [
+      {
+        accessorKey: "incidentId",
+        header: "Incident ID",
+      },
+      {
+        accessorKey: "region",
+        header: "Region",
+      },
+      {
+        accessorKey: "severity",
+        header: "ACMA",
+        Cell: ({ cell }): React.ReactNode => toTitleCase(cell.getValue<string>()),
+      },
+      {
+        accessorKey: "causeOfOutage",
+        header: "Cause of outage",
+      },
+      {
+        accessorKey: "commsRecords",
+        header: "Comms records",
+        Cell: ({ cell }): React.ReactNode => (
+          <a href={cell.getValue<string>()} target="_blank" rel="noreferrer">
+            View comms record
+          </a>
+        ),
+      },
+      {
+        accessorKey: "identifiedAt",
+        header: "Identified at",
+        Cell: ({ cell }): React.ReactNode => isoToDateTime(cell.getValue<string>()),
+      },
+      {
+        accessorKey: "status",
+        header: "Status",
+      },
+    ],
+    []
+  );
+
+  const tableProps = useMemo<MRT_TableOptions<IOutageProps>>(
+    () => ({
+      columns,
+      data: outageData?.OUTAGES || [],
+      muiTableBodyRowProps: { hover: true },
+      enableColumnActions: false,
+      enableDensityToggle: false,
+      enableFullScreenToggle: false,
+      enableHiding: false,
+      state: {
+        isLoading: loading,
+      },
+      renderTopToolbarCustomActions: () => (
+        <div className="records__label">
+          <FilterIcon />
+          Filters
+        </div>
+      ),
+    }),
+    [columns, outageData?.COMMS_RECORDS, loading]
+  );
+
+  return <MaterialReactTable {...tableProps} />;
+};
+
+export default TableOutages;
