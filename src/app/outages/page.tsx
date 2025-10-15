76955bc7580b4143c730c596908522692809a970
Nino Rodriguez <nirodriguez@deloitte.com.au>
2025-10-16 09:55:55 +1100

    feat: implement row click functionality in TableOutages to open OutageDetailPanel

--- src/app/outages/page.tsx
+++ src/app/outages/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import React, { useState } from "react";
+
 import { Card, CardContent } from "@/components/ui/card";
 
+import OutageDetailPanel from "../components/molecules/OutageDetailPanel";
 import TableOutages from "../components/molecules/TableOutages";
 import { Navigation } from "../components/organisms/Navigation";
 import { DataProvider } from "../contexts/DataContext";
 
 export default function Outages() {
+  const [panelOpen, setPanelOpen] = useState(false);
+  const [incidentId, setIncidentId] = useState("");
+
   return (
     <DataProvider>
       <main className="main records records-container">
@@ -18,7 +24,18 @@ export default function Outages() {
 
         <Card className="records-card">
           <CardContent className="records-card__content">
-            <TableOutages />
+            <TableOutages
+              onRowClick={(arg) => {
+                setPanelOpen(true);
+                setIncidentId(arg);
+              }}
+            />
+
+            <OutageDetailPanel
+              incidentId={incidentId}
+              open={panelOpen}
+              onClose={() => setPanelOpen(false)}
+            />
           </CardContent>
         </Card>
       </main>
