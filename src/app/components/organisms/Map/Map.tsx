19dd138f710f51ff70018107c007f40f52fabb8c
Nino Rodriguez <nirodriguez@deloitte.com.au>
2025-10-16 12:19:50 +1100

    feat: implement click handling in Map component and integrate OutageDetailPanel

--- src/app/components/organisms/Map/Map.tsx
+++ src/app/components/organisms/Map/Map.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { useDataOutage } from "@/app/contexts/DataContext";
 import { Card } from "@/components/ui/card";
 import { formatCreationTime } from "@/lib/utils";
@@ -5,14 +7,16 @@ import { formatCreationTime } from "@/lib/utils";
 import "./Map.styles.scss";
 import { IMapProps } from "./Map.types";
 import { GoogleMap } from "../../molecules/GoogleMap";
+import OutageDetailPanel from "../../molecules/OutageDetailPanel";
 import { OutageStats } from "../../molecules/OutageStats";
 import { OutageTabs } from "../OutageTabs";
 
 export const Map = (props: IMapProps) => {
-  const { id } = props;
-
+  const [panelOpen, setPanelOpen] = useState(false);
+  const [incidentId, setIncidentId] = useState("");
   const { outageData, loading } = useDataOutage();
 
+  const { id } = props;
   return (
     <div id={id} className="map">
       <Card className="map__card">
@@ -34,7 +38,19 @@ export const Map = (props: IMapProps) => {
           <GoogleMap center={{ lat: -25.7326, lng: 134.4895 }} zoom={4} />
         </Card>
 
-        <OutageTabs id="outage-tabs" />
+        <OutageTabs
+          id="outage-tabs"
+          onCardClick={(incidentId) => {
+            setPanelOpen(true);
+            setIncidentId(incidentId);
+          }}
+        />
+
+        <OutageDetailPanel
+          incidentId={incidentId}
+          open={panelOpen}
+          onClose={() => setPanelOpen(false)}
+        />
       </Card>
     </div>
   );
